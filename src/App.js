4740afce491f2f87565b64417fd2be9bf2e2e691
santoshi-1 <karuturisantoshi@gmail.com>
2021-08-03 22:10:41 +0530

    added AutoComplete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import React, { useState } from "react";
+import Autocomplete from "react-autocomplete";
+
 function App() {
+  const [value, setValue] = useState();
+
   return (
     <div className="App">
-      Hello World!
+      <Autocomplete
+        items={[
+          { id: "foo", label: "foo" },
+          { id: "bar", label: "bar" },
+          { id: "baz", label: "baz" },
+        ]}
+        shouldItemRender={(item, value) =>
+          item.label.toLowerCase().indexOf(value.toLowerCase()) > -1
+        }
+        getItemValue={(item) => item.label}
+        renderItem={(item, highlighted) => (
+          <div
+            key={item.id}
+            style={{ backgroundColor: highlighted ? "#eee" : "transparent" }}
+          >
+            {item.label}
+          </div>
+        )}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        onSelect={(value) => setValue(value)}
+      />
     </div>
   );
 }
