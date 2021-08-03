81a9755d7782d7dba105ea49e02eae4ebb30fccd
santoshi-1 <karuturisantoshi@gmail.com>
2021-08-03 23:29:48 +0530

    using axios to get data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import Autocomplete from "react-autocomplete";
 
 function App() {
-  const [value, setValue] = useState();
+  const [value, setValue] = useState('');
+
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=${value}`
+      )
+      .then(function (response) {
+        const parsedResponse = [];
+
+        for (let i = 0; i < response.data[1].length; i++) {
+          parsedResponse.push({
+            id: response.data[3][i],
+            label: response.data[1][i],
+          });
+        }
+        setItems(parsedResponse);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, [value]);
 
   return (
     <div className="App">
       <Autocomplete
-        items={[
-          { id: "foo", label: "foo" },
-          { id: "bar", label: "bar" },
-          { id: "baz", label: "baz" },
-        ]}
+        items={items}
         shouldItemRender={(item, value) =>
           item.label.toLowerCase().indexOf(value.toLowerCase()) > -1
         }
