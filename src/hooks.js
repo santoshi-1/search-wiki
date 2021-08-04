6ae5d98d397ce5dbbfc9f77e8fedf51181bcea61
santoshi-1 <karuturisantoshi@gmail.com>
2021-08-04 09:30:26 +0530

    added common search hook

---
+++ src/hooks.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+
+const useSearch = (query) => {
+  const [state, setState] = useState({
+    articles: [],
+    status: "IDLE",
+    error: "",
+  });
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=${query}`
+      )
+      .then(function (response) {
+        const parsedResponse = [];
+
+        for (let i = 0; i < response.data[1].length; i++) {
+          parsedResponse.push({
+            id: response.data[3][i],
+            label: response.data[1][i],
+          });
+        }
+        setState({
+          articles: parsedResponse,
+          status: "SUCCESS",
+          error: "",
+        });
+      })
+      .catch(function (error) {
+        setState({
+          articles: [],
+          status: "ERROR",
+          error: error,
+        });
+      });
+  }, [query]);
+
+  return state;
+};
+
+export default useSearch;
